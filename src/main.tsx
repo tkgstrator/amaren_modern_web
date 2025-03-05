dba771aa27d961731311e25c3f8a728bb39e8a0d
tkgstrator <nasawake.am@gmail.com>
2025-03-05 13:39:09 +0000

    refactor(theme): replace ThemeProvider with CssVarsProvider from @mui/joy

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,17 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import { ThemeProvider, createTheme } from '@mui/material'
+import { CssVarsProvider } from '@mui/joy'
 import { StrictMode, Suspense } from 'react'
 import App from './App.tsx'
 import FallbackView from './views/FallbackView.tsx'
 
-const theme = createTheme({})
-
 // biome-ignore lint/style/noNonNullAssertion: <explanation>
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ThemeProvider theme={theme}>
+    <CssVarsProvider disableTransitionOnChange>
       <Suspense fallback={<FallbackView />}>
         <App />
       </Suspense>
-    </ThemeProvider>
+    </CssVarsProvider>
   </StrictMode>
 )
