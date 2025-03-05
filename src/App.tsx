6363a69958c16c92c620909e666881bf51ce9e15
tkgstrator <tkgstrator@users.noreply.github.com>
2025-03-05 13:58:03 +0000

    feat(app): update dependencies, add components, and configure i18n and dayjs

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,29 @@
+import { Box } from '@mui/joy'
 import type React from 'react'
-import './utils/i18n'
+import { useEffect } from 'react'
 import { useTranslation } from 'react-i18next'
+import Header from './components/Header'
+import Sidebar from './components/Sidebar'
+import { Logger } from './utils/logger'
 
-const App: React.FC<React.HTMLAttributes<HTMLDivElement>> = ({ children, onClick, ...props }) => {
+const App: React.FC<React.HTMLAttributes<HTMLDivElement>> = () => {
   const { i18n } = useTranslation()
 
-  return <></>
+  useEffect(() => {
+    i18n.changeLanguage('ja')
+  }, [i18n])
+
+  useEffect(() => {
+    Logger.info('CROSS ORIGIN ISOLATED', window.crossOriginIsolated)
+    Logger.info('DEV', import.meta.env.DEV)
+  }, [])
+
+  return (
+    <Box>
+      <Header />
+      <Sidebar />
+    </Box>
+  )
 }
 
 export default App
