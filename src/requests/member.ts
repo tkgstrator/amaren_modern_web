8f19a419a67e1ecca42634274d110f8d118a5bf2
tkgstrator <tkgstrator@users.noreply.github.com>
2025-03-06 01:34:41 +0000

    refactor(devcontainer): update container name and feature versions, adjust API path

--- src/requests/member.ts
+++ src/requests/member.ts
@@ -4,7 +4,7 @@ import type { HTTPHeaders, HTTPParameters, RequestType } from './request_type'
 
 export class MemberQuery implements RequestType {
   method = HTTPMethod.POST
-  path = 'api/members'
+  path = 'members'
   headers?: HTTPHeaders | undefined = undefined
   body?: HTTPParameters | undefined = undefined
   encoding?: ParameterEncoding | undefined = ParameterEncoding.JSON
