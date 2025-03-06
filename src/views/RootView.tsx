cb0a95ed4290c750471f6ea383b56d4f67f0f423
tkgstrator <nasawake.am@gmail.com>
2025-03-06 05:14:40 +0000

    feat(RootView): add warning alert for partially implemented features

--- src/views/RootView.tsx
+++ src/views/RootView.tsx
@@ -1,4 +1,5 @@
-import { Box, Typography } from '@mui/joy'
+import { Warning } from '@mui/icons-material'
+import { Alert, Box, Typography } from '@mui/joy'
 import NavigationHeader from '../components/Navigation'
 
 const RootView: React.FC = () => {
@@ -6,8 +7,18 @@ const RootView: React.FC = () => {
     <>
       <NavigationHeader>
         <Box>
-          <Typography>ここは一般社団法人日本アマチュア将棋連盟の非公式ウェブサイトです。</Typography>
-          <Typography>大会結果速報や登録者のレーティングなどを確認することができます。</Typography>
+          <Box
+            sx={{
+              mb: 2
+            }}
+          >
+            <Typography>ここは一般社団法人日本アマチュア将棋連盟の非公式ウェブサイトです。</Typography>
+            <Typography>大会結果速報や登録者のレーティングなどを確認することができます。</Typography>
+            <Typography>公式サイトよりも高速に閲覧、検索ができます。ご自由にご利用ください。</Typography>
+          </Box>
+          <Alert startDecorator={<Warning />}>
+            <Typography>一部の機能は現在実装中です。</Typography>
+          </Alert>
         </Box>
       </NavigationHeader>
     </>
